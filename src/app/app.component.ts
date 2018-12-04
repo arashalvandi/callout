db31f7da5fc13c88e148ef1398a46aafb59f7d8f
= <=>
2018-12-04 11:33:38 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterContentInit, ElementRef, ViewChild } from '@angular/core';
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterContentInit {
+  @ViewChild('anchor') buttonRef: ElementRef<HTMLElement>;
   title = 'app';
+
+  constructor(private overlay: Overlay) {}
+
+  menuPortal: ComponentPortal<MenuComponent>;
+  overlayRef: OverlayRef;
+  overlayRef2: OverlayRef;
+
+  ngOnInit() {
+  }
+
+  ngAfterContentInit() {
+    console.log(this.buttonRef);
+    const positionStrategy = this.overlay.position()
+      .connectedTo(this.buttonRef, 
+      { originX: 'start', originY: 'bottom' },
+      { overlayX: 'start', overlayY: 'top' }
+    );
+
+    this.overlayRef = this.overlay.create({
+      positionStrategy
+    });
+
+    this.menuPortal = new ComponentPortal(MenuComponent);
+  }
+  
+  openDialog() {
+    if (this.overlayRef.hasAttached()) {
+      this.overlayRef.detach();
+    } else {
+      this.overlayRef.attach(this.menuPortal);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OverlayModule } from '@angular/cdk/overlay';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    OverlayModule
   ],
   providers: [],
+  entryComponents: [MenuComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
